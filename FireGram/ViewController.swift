074f9c332a772ecf4d6fb93fc326674ff8da506d
MDeLaCruz92 <delacruzm10@gmail.com>
2017-07-29 21:11:31 -0400

    Initial Commit

---
+++ FireGram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireGram
+//
+//  Created by Michael De La Cruz on 7/29/17.
+//  Copyright © 2017 Michael De La Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
